dfcdac3321f4226a28bfdefb2279eca0025d38fe
JizongL <2015rpro@gmail.com>
2019-07-09 15:43:59 -0400

    first commit

---
+++ prototype.js
@@ -0,0 +1,14 @@
+dog 
+color
+breed
+age
+
+function dogFile(color,breed,age,location){
+    this.color = color;
+    this.breed = breed;
+    this.age = age;
+    this.location = location
+    
+}
+
+const dog_1 = new dogFile("red","dalmatian","3","NYC")
